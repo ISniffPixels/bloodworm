9249a159f5ac2e13218430a1e2f9299c7c39a796
ISniffPixels <hugowinged@gmail.com>
2024-10-28 03:18:49 -0700

    added highscore logic and collision detect function to JS file

--- bloodworm.js
+++ bloodworm.js
@@ -6,6 +6,7 @@ const instructions = document.querySelector("#instruction_text");
 const logo = document.querySelector(".bloodworm-logo");
 
 // DEFINING GAME VARIABLES
+const score = document.querySelector("#score");
 let gridArea = 20;
 let bloodWorm = [{ x: 10, y: 10 }];
 let prey = generatePrey();
@@ -19,6 +20,7 @@ function draw() {
   board.innerHTML = "";
   drawBloodWorm();
   drawPrey();
+  scoreUpate();
 }
 
 // DRAW BLOODWORM
@@ -80,10 +82,12 @@ function move() {
 
   if (bloodWormHead.x === prey.x && bloodWormHead.y === prey.y) {
     prey = generatePrey();
+    increaseSpeed();
     // CLEARS PREVIOUS INTERVALS
-    clearInterval();
+    clearInterval(gameInterval);
     gameInterval = setInterval(() => {
       move();
+      collisionDetect();
       draw();
     }, gameSpeedDelay);
   } else {
@@ -94,13 +98,81 @@ function move() {
 // STARTS GAME
 function startGame() {
   gameStart = true;
-  // instructions.style.display = "none";
-  // logo.style.display = "none";
+  instructions.style.display = "none";
+  logo.style.display = "none";
   gameInterval = setInterval(() => {
     move();
-    // collisionCheck();
+    collisionDetect();
     draw();
   }, gameSpeedDelay);
 }
 
 // SPACEBAR KEY WILL INITIATE GAME USING ADDEVENTLISTENER
+function initSession(e) {
+  if ((!gameStart && e.code === "space") || (!gameStart && e.key === " ")) {
+    startGame();
+  } else {
+    if (e.key === "ArrowUp") {
+      direction = "up";
+    } else if (e.key === "ArrowRight") {
+      direction = "right";
+    } else if (e.key === "ArrowDown") {
+      direction = "down";
+    } else if (e.key === "ArrowLeft") {
+      direction = "left";
+    }
+  }
+}
+
+document.addEventListener("keydown", initSession);
+function increaseSpeed() {
+  console.log(gameSpeedDelay);
+  // CONDITIONALS MANAGE THE SPEED OF BLOODWORM THE MORE IT EATS SO IT  DOESN'T GET OUT OF CONTROL AND BECOME HARD TO MOVE
+  if (gameSpeedDelay > 150) {
+    gameSpeedDelay -= 5;
+  } else if (gameSpeedDelay > 100) {
+    gameSpeedDelay -= 3;
+  } else if (gameSpeedDelay > 50) {
+    gameSpeedDelay -= 2;
+  } else if (gameSpeedDelay > 25) {
+    gameSpeedDelay -= 1;
+  }
+}
+
+// CHECKS TO SEE IF THERE IS A COLLISION AND RESETS GAME IF BLOODWORM COLLIDES WITH SURROUNDINGS
+function collisionDetect() {
+  const bloodWormHead = bloodWorm[0];
+
+  if (
+    bloodWormHead.x < 1 ||
+    bloodWormHead.x > gridArea ||
+    bloodWormHead.y < 1 ||
+    bloodWormHead.y > gridArea
+  ) {
+    resetGame();
+  }
+
+  for (let s = 1; s < bloodWormHead.length; s++) {
+    if (
+      bloodWormHead.x === bloodWorm[s].x &&
+      bloodWormHead.y === bloodWorm[s].y
+    ) {
+      resetGame();
+    }
+  }
+}
+
+// RESETS GAME FUNCTION CALL
+function resetGame() {
+  bloodWorm = [{ x: 10, y: 10 }];
+  prey = generatePrey();
+  direction = "right";
+  gameSpeedDelay = 200;
+  scoreUpate();
+}
+
+// UPDATES SCORE UPON GAME RESET
+function scoreUpate() {
+  const currentScore = bloodWorm.length - 1;
+  score.innerHTML = currentScore.toString().padStart(3, "0");
+}
